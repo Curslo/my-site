4a4408e8056b658c926cd2955205d7468739ab7f
Curslo <richardkisivii@gmail.com>
2024-01-14 17:33:51 +0300

    Projects Page Navigation Bar

--- src/app/Projects/page.tsx
+++ src/app/Projects/page.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 import ProjectsNavBar from '@/components/ProjectsNavBar'
 import ProjectsCard from '@/components/ProjectsCard'
-import { projects } from '../../../data/Projects'
+import { projects as ProjectData } from '../../../data/Projects'
+import { Category } from '../../../types/page'
 const Projects = () => {
+
+const [projects, setProjects] = useState(ProjectData);
+const [active, setActive] = useState('All');
+const filterCategory = (category: Category | 'All') => {
+  if(category === 'All') {
+    setProjects(ProjectData);
+    setActive(category);
+    return;
+  }
+  const newArray = ProjectData.filter((project) => project.category.includes(category))
+  setProjects(newArray);
+  setActive(category)
+}
+
   return (
     <div className='px-5 py-2 overflow-y-scroll' style={{height:'65vw'}}>
-      <ProjectsNavBar/>
+      <ProjectsNavBar filterCategory= {filterCategory} active = {active} />
       <div className='grid grid-cols-12 gap-4 my-3 relative'>
         {/* List of Projects done */}
         {projects.map((project) => (
--- src/components/ProjectsNavBar.tsx
+++ src/components/ProjectsNavBar.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { FunctionComponent } from 'react'
+import { Category } from '../../types/page'
 
-const ProjectsNavBar = () => {
+export const ProjectsNavItem: FunctionComponent<{data: Category | 'All', filterCategory:Function, active: string}> = ({data, filterCategory, active}) => {
+
+  let className='cursorpointer capitalize hover:text-green-500'
+  if(active === data) {className += ' text-green-500'}
   return (
-    <div>ProjectsNavBar</div>
+    <li className={className} onClick={() => filterCategory(data)}>{data}</li>
+  )
+}
+
+
+const ProjectsNavBar: FunctionComponent<{filterCategory: Function, active: string}> = (props) => {
+  return (
+    <div className='px-3 py-2 space-x-3 overflow-x-auto list-none flex'>
+      <ProjectsNavItem data='All'{...props}/>
+      <ProjectsNavItem data='CSS'{...props}/>
+      <ProjectsNavItem data='Express.js'{...props}/>
+      <ProjectsNavItem data='Firebase'{...props}/>
+      <ProjectsNavItem data='HTML'{...props}/>
+      <ProjectsNavItem data='Javascript'{...props}/>
+      <ProjectsNavItem data='Mongodb'{...props}/>
+      <ProjectsNavItem data='Nest.js'{...props}/>
+      <ProjectsNavItem data='Next.js'{...props}/>
+      <ProjectsNavItem data='Node.js'{...props}/>
+      <ProjectsNavItem data='React.js'{...props}/>
+      <ProjectsNavItem data='SQL'{...props}/>
+
+    </div>
   )
 }
 
