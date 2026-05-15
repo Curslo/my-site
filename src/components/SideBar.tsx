30d1b5b036bcb855e44c986cda44dda15894a278
Curslo <richardkisivii@gmail.com>
2026-05-15 03:46:53 +0300

    Update Resume file and path

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -39,7 +39,7 @@ export function SideBar() {
       </p>
       <a
         className="px-2 py-1 my-3 flex items-center justify-center bg-gray-200  dark:bg-gray-900 rounded-full"
-        href="/assets/My Software Development Resume.pdf"
+        href="/assets/Richard Kisivii Resume.pdf"
         download={"Richard Kisivii"}
       >
         <GiTie className="w-6 h-6" />
